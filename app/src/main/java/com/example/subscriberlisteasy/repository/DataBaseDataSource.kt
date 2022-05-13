9aaabd75924bc858e26bf39a8ce89e99ab24edf8
Wesley Vila Nova <wesleyvilanovadelimatorres@gmail.com>
2022-05-13 18:46:22 -0300

    create repository DataBase

---
+++ app/src/main/java/com/example/subscriberlisteasy/repository/DataBaseDataSource.kt
@@ -0,0 +1,36 @@
+package com.example.subscriberlisteasy.repository
+
+import androidx.lifecycle.LiveData
+import com.example.subscriberlisteasy.data.db.dao.SubscriberDAO
+import com.example.subscriberlisteasy.data.db.entity.SubscriberEntity
+
+class DataBaseDataSource(private val subscriberDAO: SubscriberDAO) : ISubscriberRepository {
+    override suspend fun insertSubscriber(name: String, email: String): Long {
+        val subscriberInsert = SubscriberEntity(
+            name = name,
+            email = email
+        )
+        return subscriberDAO.insert(subscriberInsert)
+    }
+
+    override suspend fun updateSubscriber(id: Long, name: String, email: String) {
+        val subscriberUpdate = SubscriberEntity(
+            id = id,
+            name = name,
+            email = email
+        )
+        return subscriberDAO.update(subscriberUpdate)
+    }
+
+    override suspend fun deleteSubscriber(id: Long) {
+        return subscriberDAO.delete(id)
+    }
+
+    override suspend fun deleteAllSubscriber() {
+        subscriberDAO.getAll()
+    }
+
+    override suspend fun getAllSubscriber(): LiveData<List<SubscriberEntity>> {
+        return subscriberDAO.getAll()
+    }
+}
---
+++ app/src/main/java/com/example/subscriberlisteasy/repository/ISubscriberRepository.kt
@@ -0,0 +1,17 @@
+package com.example.subscriberlisteasy.repository
+
+import androidx.lifecycle.LiveData
+import com.example.subscriberlisteasy.data.db.entity.SubscriberEntity
+
+interface ISubscriberRepository {
+
+    suspend fun insertSubscriber(name: String, email: String): Long
+
+    suspend fun updateSubscriber(id: Long, name: String, email: String)
+
+    suspend fun deleteSubscriber(id: Long)
+
+    suspend fun deleteAllSubscriber()
+
+    suspend fun getAllSubscriber(): LiveData<List<SubscriberEntity>>
+}
