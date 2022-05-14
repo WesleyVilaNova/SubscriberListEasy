5ab12b7ba104cd64a943dcb1bd5c9f7ecd58c536
Wesley Vila Nova <wesleyvilanovadelimatorres@gmail.com>
2022-05-14 09:35:22 -0300

    create screen animation

---
+++ app/src/main/java/com/example/subscriberlisteasy/extension/NavExtension.kt
@@ -0,0 +1,23 @@
+package com.example.subscriberlisteasy.extension
+
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import com.example.subscriberlisteasy.R
+
+
+// Animção de tela do fragment
+
+
+private val slideLeftOptions = NavOptions.Builder()
+    .setEnterAnim(R.anim.slide_in_right) // animação de entrada
+    .setExitAnim(R.anim.slide_out_left) // animação de saída
+    .setPopEnterAnim(R.anim.slide_in_left) // animação de pop de entrada
+    .setPopExitAnim(R.anim.slide_out_right) // animação pop de saída
+    .build()
+
+fun NavController.navigateWithAnimations(
+    destinationId: Int,
+    animation: NavOptions = slideLeftOptions
+) {
+    this.navigate(destinationId, null, animation)
+}
