26b307ca723a8f3e5855869dc402f523e9e7af3d
Wesley Vila Nova <wesleyvilanovadelimatorres@gmail.com>
2022-05-14 07:22:54 -0300

    create screen item recyclerview

---
+++ app/src/main/java/com/example/subscriberlisteasy/uisubscriberlist/SubscriberListFragment.kt
@@ -0,0 +1,13 @@
+package com.example.subscriberlisteasy.uisubscriberlist
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.subscriberlisteasy.R
+
+class SubscriberListFragment : Fragment(R.layout.subscriber_list_fragment) {
+
+}
